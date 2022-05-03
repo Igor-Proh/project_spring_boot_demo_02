4aceae2e8fd59d54c3358682c770bef81e19077f
Igor-Proh <71402291+Igor-Proh@users.noreply.github.com>
2022-05-03 19:35:48 +0300

    Add files via upload

---
+++ project_springboot_demo_02_09_data_rest/src/main/java/com/prokhnov/springboot/cruddemo/dao/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.prokhnov.springboot.cruddemo.dao;
+
+import com.prokhnov.springboot.cruddemo.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+//@RepositoryRestResource(path = "members")
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+}
